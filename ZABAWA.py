dbf439fb26983e82e20101c703c73680cc7f6606
DJK444 <131398422+DJK444@users.noreply.github.com>
2024-04-11 21:45:08 +0200

    Add files via upload

---
+++ ZABAWA.py
@@ -0,0 +1,14 @@
+#%%
+print("marchewka",end=", ")
+print("marchewka",end=", ")
+print("marchewka",end=", ")
+print("marchewka",end=", ")
+print("marchewka",end=", ")
+print("marchewka",end=", ") #na koncu zdania mam przecinek zamiast entera
+#%%
+print("marchewka")
+
+
+
+
+# %%
